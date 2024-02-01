fbe1b8bb460f938d47499a3506ff1a6f6b7b34f3
Nando1702 <120483561+Nando1702@users.noreply.github.com>
2024-02-01 16:17:05 +0100

    commit 1, mejora del formato de salida commit 2, programa encapsulado en metodos + resolucion de problemas commit 3, dado implementacion commit 4, casillas DAO

---
+++ src/main/java/pkg_DAO/CasillasDao.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg_DAO;
+
+import java.util.List;
+import modelo.Casillas;
+
+/**
+ *
+ * @author usuario
+ */
+public class CasillasDao implements ICasillasDao{
+
+    @Override
+    public void saveOrUpdate(Casillas entity) {
+        
+    }
+
+    @Override
+    public Casillas get(Integer id) {
+        
+        return null;
+        
+    }
+
+    @Override
+    public void delete(Integer id) {
+        
+     }
+
+    @Override
+    public void delete(Casillas entity) {
+        
+    }
+
+    @Override
+    public List<Casillas> list() {
+        
+        return null;
+        
+    }
+    
+}
---
+++ src/main/java/pkg_DAO/ICasillasDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pkg_DAO;
+
+import modelo.Casillas;
+
+/**
+ *
+ * @author usuario
+ */
+public interface ICasillasDao extends IDAOGeneric<Casillas,Integer> {
+    
+}
